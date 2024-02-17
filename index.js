4212484eeb16a070c2c90e509965796ee80422f9
Manish03Singh <manish031627@gmail.com>
2024-02-17 13:53:46 +0530

    new chnages

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
 app.use('/',router)
 DBConnection();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
 })
